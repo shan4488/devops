7dd374613f4264f722e77292263f8fd88057d765
Dharithri Rai B <dharithri.rai.b@sap.com>
2024-06-01 15:40:34 +0530

    added orders route

--- server/index.js
+++ server/index.js
@@ -4,15 +4,19 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./routes/User.js";
 import FoodRoutes from "./routes/Food.js";
+// import orderRoutes from "./routes/Orders.js";
+
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
-
 app.use("/api/user/", UserRoutes);
 app.use("/api/food/", FoodRoutes);
+// app.use('/api/Orders', orderRoutes);
+
 
 // error handler
 app.use((err, req, res, next) => {
