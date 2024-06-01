525d7c4a340bc7492635e90f5c97fed0c32409dc
kabir1001ribak <2023sl93023@wilp.bits-pilani.ac.in>
2024-06-01 11:16:30 +0530

    Adding CORS orgin

--- server/index.js
+++ server/index.js
@@ -7,8 +7,12 @@ import FoodRoutes from "./routes/Food.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(
+  cors({
+    origin: "http://localhost:3000", // <-- location of the frontend app were connecting to
+    credentials: true,
+  })
+);app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
 app.use("/api/user/", UserRoutes);
